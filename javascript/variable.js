5e3af07e8c37f50db8f1b4fea2c0b9cf184fae9c
vinit mittal <vinitmittal295@gmail.com>
2024-04-24 12:34:59 +0530

    file added

---
+++ javascript/variable.js
@@ -0,0 +1,34 @@
+// console.log("hello world")
+// var a=10;
+// console.log(a)
+
+// let b=20;
+// b=50;
+// console.log(b)
+
+// const c=40;
+// c=50;
+// console.log(c)
+
+
+{
+    var a=10;  // global variable
+    let b=20;   // local variable 2015 es6
+    const c=10; // local variable 2015 es6
+    d=10; //var default
+     
+}
+
+console.log(c)
+console.log(b)
+console.log(a)
+console.log(d)
+
+var a=10;
+
+for(let a=0; a<5; a++){
+    console.log(a)
+}
+console.log()
+
+
