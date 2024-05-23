fa3b0bb8c866fcb68592f06ed8de11297ec36d9c
vinit mittal <vinitmittal295@gmail.com>
2024-05-23 11:41:09 +0530

    added

---
+++ javascript/fxnpractise.js
@@ -0,0 +1,5 @@
+let a= 10;
+function vinit(){
+    let b=20
+}
+console.log(vinit())
---
+++ javascript/objectpr.js
@@ -0,0 +1,56 @@
+// myobj= {
+//     name:"john",
+//     age:"30",
+//     mycars: {   
+//         car1:"ford",
+//         car2:"bmw",
+//         car3:"fiat",
+//     }
+// }
+// console.log(myobj.age);
+// console.log(myobj.mycars.car3)
+// console.log(myobj.mycars["car3"])
+// console.log(myobj["mycars"]["car3"])
+// delete myobj["age"]
+// console.log(myobj.age)
+
+
+// const person = new Object();
+
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+// console.log(person.firstName)
+// console.log(person.lastName)
+
+// const person=new Object()
+// person.firstName="vinit"
+// person.lastName="mittal"
+// person.age=25
+// person.eyeColor="blue"
+
+// console.log(person.firstName)
+
+// const person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
+//   console.log(person.fullName())
+
+
+//   const person={
+//     firstname:"vinit",
+//     lastname:"mittal",
+//     age:30,
+//     eyecolor:"red",
+//   }
+//   const x=person;
+//   console.log(x.age)
+
+
